e55959b5c228e1bc39d482e2d0d0fe5f3d9b12a7
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-23 18:54:13 +0900

    feat: #3 - 메인 클래스에 Stadium 관련 클래스 의존성 추가

--- src/main/java/BaseBallApp.java
+++ src/main/java/BaseBallApp.java
@@ -1,4 +1,15 @@
+import dao.StadiumDAO;
+import db.DBConnection;
+import service.StadiumService;
+
 public class BaseBallApp {
+
+    private static final StadiumService stadiumService;
+
+    static {
+        stadiumService = new StadiumService(new StadiumDAO(DBConnection.getInstance()));
+    }
+
     public static void main(String[] args) {
 
     }
