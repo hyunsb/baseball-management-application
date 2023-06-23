e956ca5fa9780467f51168c62281a6f7e1b9d5a3
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-23 18:52:35 +0900

    feat: #3 - StadiumService 구현 사용자로부터 전달받은 요청 오브젝트를 모델로 파싱한다. 요청에 대한 작업을 수행한 뒤 응답 오브젝트를 생성하여 반환한다.

---
+++ src/main/java/service/StadiumService.java
@@ -0,0 +1,83 @@
+package service;
+
+import dao.StadiumDAO;
+import dto.stadium.StadiumRequest;
+import dto.stadium.StadiumResponse;
+import model.Stadium;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class StadiumService {
+
+    private final StadiumDAO stadiumDAO;
+
+    public StadiumService(StadiumDAO stadiumDAO) {
+        this.stadiumDAO = stadiumDAO;
+    }
+
+    // 야구장 저장
+    public StadiumResponse save(StadiumRequest request) throws IllegalArgumentException {
+        String name = request.getName();
+        Optional<Stadium> result;
+
+        try {
+            result = stadiumDAO.createStadium(name);
+        } catch (SQLException exception) {
+            throw new IllegalArgumentException("SQL 에러 [insert]: " + exception.getMessage());
+        }
+        Stadium stadium = result.orElseThrow(() -> new IllegalArgumentException("Stadium save 실패"));
+
+        return StadiumResponse.from(stadium);
+    }
+
+    // 전체 야구장 검색
+    public List<StadiumResponse> findAll() {
+        try {
+            List<Stadium> stadiums = stadiumDAO.findAll();
+            return stadiums.stream()
+                    .map(StadiumResponse::from)
+                    .collect(Collectors.toList());
+
+        } catch (SQLException exception) {
+            throw new IllegalArgumentException("SQL 에러 [select All]: " + exception.getMessage());
+        }
+    }
+
+    // 야구장 이름으로 검색
+    public StadiumResponse findByName(StadiumRequest request) throws IllegalArgumentException {
+        String name = request.getName();
+        Optional<Stadium> result;
+
+        try {
+            result = stadiumDAO.findStadiumByName(name);
+        } catch (SQLException exception) {
+            throw new IllegalArgumentException("SQL 에러 [select]: " + exception.getMessage());
+        }
+        Stadium stadium = result.orElseThrow(() -> new IllegalArgumentException(name + "에 해당하는 경기장이 존재하지 않습니다."));
+
+        return StadiumResponse.from(stadium);
+    }
+
+    // 야구장 이름으로 삭제
+    public void deleteByName(StadiumRequest request) {
+        String name = request.getName();
+
+        try {
+            stadiumDAO.deleteById(name);
+        } catch (SQLException exception) {
+            throw new IllegalArgumentException("SQL 에러 [delete]: " + exception.getMessage());
+        }
+    }
+
+    // 전체 야구장 삭제
+    public void deleteAll() {
+        try {
+            stadiumDAO.deleteAll();
+        } catch (SQLException exception) {
+            throw new IllegalArgumentException("SQL 에러 [delete All]: " + exception.getMessage());
+        }
+    }
+}
