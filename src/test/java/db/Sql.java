0cdd54199bdab0cac0fc5775bc3c8c7231a51dcc
jy-b <jongyoonb+u@gmail.com>
2023-06-27 07:39:10 +0900

    feat: #9 - Sql 열거체 수정
    OUT_PLAYER 추가

--- src/test/java/db/Sql.java
+++ src/test/java/db/Sql.java
@@ -6,13 +6,21 @@ public enum Sql {
             "    name varchar(20) not null,\n" +
             "    position varchar(20) not null,\n" +
             "    created_at timestamp not null DEFAULT CURRENT_TIMESTAMP,\n" +
-            "    team_id bigint,\n" +
+            "    team_id bigint null,\n" +
             "    foreign key (team_id) references team(id),\n" +
             "\t\tunique key (position, team_id) #다중 유니크 제약\n" +
-            ") ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;", "DROP TABLE if exists player");
+            ") ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;", "DROP TABLE if exists player"),
 
-    private String create;
-    private String drop;
+    OUT_PLAYER("create table out_player (\n" +
+           "    id bigint primary key auto_increment,\n" +
+           "    reason varchar(45) unique not null,\n" +
+           "    player_id bigint,\n" +
+           "    created_at timestamp not null DEFAULT CURRENT_TIMESTAMP,\n" +
+           "    foreign key (player_id) references player(id)\n" +
+           "    ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;", "DROP TABLE if exists out_player");
+
+    private final String create;
+    private final String drop;
 
     Sql(String create, String drop) {
         this.create = create;
