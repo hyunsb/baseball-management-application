74d163332915b96828b9d359e280731d099aa6ab
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-06-29 01:38:27 +0900

    feat: #6 - response 출력 기능 구현 리스트를 출력하는 기능과 하나의 오브젝트만 출력하는 기능으로 나누어 구현함 여러 response를 인자로 받을 수 있도록 Object, 와일드카드로 타입 선언함 Object 타입으로 인자를 받기 때문에 toString()을 재정의하여 정보를 출력하도록 구현함

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,20 +1,52 @@
 package view;
 
 import domain.Request;
+import exception.BadRequestException;
 import util.Console;
 import util.RequestParser;
+
+import java.util.List;
 
 public class View {
 
     private static final String INPUT_REQUEST = "어떤 기능을 요청하시겠습니까?";
+
+    private static final String REQUEST_SUCCESS_RESULT_FORMAT = "\n[%s]\n%s\n";
 
     // Suppresses default constructor, ensuring non-instantiability
     private View() {
     }
 
     public static Request inputRequest() {
-        System.out.println(INPUT_REQUEST);
-        String consoleRequest = Console.readLine();
-        return RequestParser.parse(consoleRequest);
+        try {
+            System.out.println(INPUT_REQUEST);
+            String consoleRequest = Console.readLine();
+            return RequestParser.parse(consoleRequest);
+
+        } catch (BadRequestException exception) {
+            printErrorMessage(exception.getMessage());
+            return inputRequest();
+        }
+    }
+
+    public static void printResponse(String request, final Object response) {
+        String result = String.format(REQUEST_SUCCESS_RESULT_FORMAT, request, response);
+        System.out.println(result);
+    }
+
+    public static void printResponse(String request, final List<?> responses) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (responses.isEmpty())
+            stringBuilder.append("목록이 존재하지 않습니다.");
+
+        responses.forEach(response -> stringBuilder.append(response).append("\n"));
+
+        String result = String.format(REQUEST_SUCCESS_RESULT_FORMAT, request, stringBuilder);
+        System.out.println(result);
+    }
+
+    public static void printErrorMessage(String message) {
+        System.out.println("\n" + message + "\n");
     }
 }
